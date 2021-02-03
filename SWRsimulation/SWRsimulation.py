4bc6033b58d363bfaf6af5bd6855f9bfed192f36
Druce Vertes <dv114@columbia.edu>
2021-02-03 17:42:10 -0500

    crra pytest

--- SWRsimulation/SWRsimulation.py
+++ SWRsimulation/SWRsimulation.py
@@ -32,6 +32,106 @@ class Trialdata:
     trial_df: pd.DataFrame = None
 
 
+def crra_ce(cashflows, gamma):
+    """takes a 1D numpy array, and gamma risk aversion parameter
+    returns total CRRA certainty-equivalent cash flow for specified gamma"""
+    
+    # for retirement study assume no negative cashflows
+    if np.any(np.where(cashflows < 0, 1, 0)):
+        return 0.0
+    elif gamma >= 1.0 and 0 in cashflows:
+        return 0.0
+    elif gamma == 0.0:
+        # 0 risk aversion
+        ce = np.mean(cashflows)
+    elif gamma == 1.0:
+        # log utility
+        u = np.mean(np.log(cashflows))
+        ce = np.exp(u)
+    elif gamma == 2.0:  # simple optimization
+        u2 = np.mean(1 - 1.0 / cashflows)
+        ce = 1.0 / (1.0 - u2)
+    elif gamma > 4.0:
+        # force computations as longdouble for more precision, but always return np.float
+        gamma = np.longdouble(gamma)
+        cashflows = cashflows.astype(np.longdouble)
+        gamma_m1 = gamma - 1.0
+        gamma_m1_inverse = 1.0 / gamma_m1
+        u = np.mean(gamma_m1_inverse - 1.0 / (gamma_m1 * cashflows ** gamma_m1))
+        ce = 1.0 / (1.0 - gamma_m1 * u) ** gamma_m1_inverse
+        ce = np.float(ce)
+    elif gamma > 1.0:
+        gamma_m1 = gamma - 1.0
+        gamma_m1_inverse = 1.0 / gamma_m1
+        u = np.mean(gamma_m1_inverse - 1.0 / (gamma_m1 * cashflows ** gamma_m1))
+        ce = 1.0 / (1.0 - gamma_m1 * u) ** gamma_m1_inverse
+    else:  # general formula
+        g_1m = 1 - gamma
+        u = np.mean((cashflows ** g_1m - 1.0) / g_1m)
+        ce = (g_1m * u + 1.0) ** (1.0 / g_1m)
+    return ce * len(cashflows)
+
+                                            
+
+def crra_ce_deathrate(cashflows, gamma, deathrate):
+    """ce cash flow with a mortality curve
+    cash flows = real cash flows in each year of cohort
+    gamma = risk aversion
+    death rate = % of cohort that died in each year of cohort
+    
+    compute utility of each cash flow under gamma
+    compute cumulative mean of utilities up to each year
+    convert utilities back to CE cash flows
+    each member of cohort that died in a given year experienced CE cash flow * years alive
+    """
+    # for retirement study assume no negative cashflows
+    
+    if sum(np.where(cashflows < 0, 1, 0)):
+        return 0.0
+    else:
+        # 1..lastyear
+        indices = np.indices(cashflows.shape)[0]+1
+        if gamma == 0.0:
+            # 0 risk aversion
+            ce = np.mean(cashflows)
+        elif gamma == 1.0:
+            # log utility
+            u = np.log(cashflows)
+            # cumulative mean utility
+            u_mean = np.cumsum(u)/indices
+            # cumulative mean ce cash flows
+            ce = np.exp(u_mean) * indices
+        elif gamma == 2.0:  # simple optimization
+            u2 = 1 - 1.0 / cashflows
+            u2_mean = np.cumsum(u2)/indices
+            ce = 1.0 / (1.0 - u2_mean) * indices
+        elif gamma > 4.0:
+            # force computations as longdouble for more precision, but always return np.float
+            gamma = np.longdouble(gamma)
+            cashflows = cashflows.astype(np.longdouble)
+            gamma_m1 = gamma - 1.0
+            gamma_m1_inverse = 1.0 / gamma_m1
+            u = gamma_m1_inverse - 1.0 / (gamma_m1 * cashflows ** gamma_m1)
+            u_mean = np.cumsum(u)/indices
+            ce = 1.0 / (1.0 - gamma_m1 * u_mean) ** gamma_m1_inverse
+            ce = (ce * indices).astype(float)
+        elif gamma > 1.0:
+            gamma_m1 = gamma - 1.0
+            gamma_m1_inverse = 1.0 / gamma_m1
+            u = gamma_m1_inverse - 1.0 / (gamma_m1 * cashflows ** gamma_m1)
+            u_mean = np.cumsum(u)/indices
+            ce = 1.0 / (1.0 - gamma_m1 * u_mean) ** gamma_m1_inverse
+            ce = ce * indices
+        else:  # general formula
+            gamma_1m = 1 - gamma
+            u = (cashflows ** gamma_1m - 1.0) / gamma_1m
+            u_mean = np.cumsum(u)/indices
+            ce = (gamma_1m * u_mean + 1.0) ** (1.0 / gamma_1m)
+            ce = ce * indices
+        # mortality_adjusted ce cash flows
+        madj_ce = np.sum(ce * deathrate)
+        return madj_ce
+                                                                                                                        
 class SWRsimulation:
     """simulate retirement outcomes.
 
